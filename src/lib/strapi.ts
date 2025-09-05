4a483eb4f6fa2b2ad1cfbfc3019ddf75fbcaf3a4
pksingh1508 <rajiit1212@gmail.com>
2025-09-06 00:49:02 +0530

    added single news file

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -8,7 +8,10 @@ export interface NewsItem {
     updatedAt?: string;
     views: number;
     slug: string;
-    short_desc: string;
+    short_desc?: string;
+    contents?: string;
+    tags?: string;
+    category?: string;
     news_image?: {
       data?: {
         attributes?: {
@@ -24,6 +27,9 @@ export interface NewsItem {
   views?: number;
   slug?: string;
   short_desc?: string;
+  contents?: string;
+  tags?: string;
+  category?: string;
   news_image?: {
     url: string;
   };
@@ -305,5 +311,40 @@ export async function fetchPaginatedImmigrationNews(
   } catch (error) {
     console.error("Error fetching paginated immigration news:", error);
     throw new Error("Failed to fetch paginated immigration news");
+  }
+}
+
+export async function getSingleNews(
+  token: string,
+  slug: string,
+  locale: string = "en",
+  collection: string
+): Promise<StrapiResponse> {
+  try {
+    const BASE_URL = `https://determined-unity-de531adc95.strapiapp.com/api/${collection}`;
+    
+    const response = await axios.get(BASE_URL, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      params: {
+        locale: locale,
+        "filters[slug][$eq]": slug,
+        "fields[0]": "title",
+        "fields[1]": "contents",
+        "fields[2]": "updatedAt",
+        "fields[3]": "views",
+        "fields[4]": "slug",
+        "fields[5]": "tags",
+        "fields[6]": "category",
+        "populate[news_image][fields][0]": "url",
+      },
+    });
+
+    return response.data as StrapiResponse;
+  } catch (error) {
+    console.error("Error fetching single news:", error);
+    throw new Error("Failed to fetch single news");
   }
 }
